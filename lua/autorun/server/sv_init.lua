bd90b281318eb2c3f40969dbe97346ac7d84a0d6
Colin Diener <colin@colind.me>
2017-11-05 21:54:14 -0500

    Bugfix for issue #4

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -5,8 +5,11 @@ CreateConVar("rcmv_votingduration", "120", { FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXE
 CreateConVar("rcmv_debug", 0, {FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXECUTE }, "Enable debugging messages in console for RCMV. Recommended to keep disabled.")
 
 -- Check if anything exists, if not, create it
+if not file.Exists("rcmapvote","data") then
+	file.CreateDir("rcmapvote")
+end
 if not file.Exists('rcmapvote/nextmap.txt','data') then
-	file.Write("rcmapvote/nextmap.txt", "ttt_rooftops_2016_v1")
+	file.Write("rcmapvote/nextmap.txt", "ttt_rooftops_2016_v1") -- This is a temporary throwaway map just to populate the file
 end
 if not file.Exists('rcmapvote/maplist.txt','data') then
 	file.Write('rcmapvote/maplist.txt', "gm_flatgrass\r\ngm_construct\r\nttt_terrortownexamplemap") -- Example of how to add maps
