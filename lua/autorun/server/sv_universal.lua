16f357614348b980a3fabf30a3f0b76f2f7cc7ef
shadsbot <colin@colind.me>
2018-08-22 01:07:57 -0400

    Add functionality to parse and interpret map data

--- lua/autorun/server/sv_universal.lua
+++ lua/autorun/server/sv_universal.lua
@@ -53,7 +53,7 @@ function insertScannedMaps()
 end
 
 function isBlackListed(map, localMapList)
-    for blacklistedMap in localMapList:gmatch("%S+") do 
+    for _,blacklistedMap in ipairs(localMapList) do 
         if (map == blacklistedMap) then
             return true
         end
@@ -61,12 +61,58 @@ function isBlackListed(map, localMapList)
     end 
 end
 
+function getMapData()
+    local localMapList = file.Read("rcmapvote/maplist.txt")
+    local processed = {}
+    local namesOnly = {}
+    
+    -- Split by newline, make array by delimiter " "
+    for map in localMapList:gmatch("([^\n]*)\n?") do
+        if (string.len(map) > 0) then -- may be a newline at end of file
+            table.insert(processed, string.Explode(" ",map))
+        end
+    end
+    for _,map in ipairs(processed) do
+            table.insert(namesOnly, map[1])
+    end
+    return processed,namesOnly
+end
+
+function getMapMinPlayers(map)
+    for _,mapName in ipairs(mapData) do 
+        if (mapName[1] == map) then
+            if not mapName[2] == nil and not mapName[2] == "" then
+                return mapName[2]
+            else
+                return 0
+            end
+        end
+    end
+    -- Map not found, tell the user and default to a safe number
+    ServerLog("RCMV:sv_universal.lua:getMapMaxPlayers(): Map " .. map .. " not found.")
+    return 0
+end
+function getMapMaxPlayers(map)
+    for _,mapName in ipairs(mapData) do 
+        if (mapName[1] == map) then
+            if not mapName[3] == nil and not mapName[3] == "" then
+                return mapName[3]
+            else
+                return 999
+            end
+        end
+    end
+    ServerLog("RCMV:sv_universal.lua:getMapMaxPlayers(): Map " .. map .. " not found.")
+    return 999
+end
+
 function updateMaps()
     dbg("updateMaps() has run")
     usableMaps = {} -- global, "update" is really a re-write from scratch
-    local localMapList = file.Read("rcmapvote/maplist.txt")
+    processed,localMapList = getMapData()
+    mapData = processed;
     if isWhitelistMode() then
-        for map in localMapList:gmatch("%S+") do 
+        for _,map in ipairs(localMapList) do 
             table.insert(usableMaps, map) 
         end
     else
@@ -87,3 +133,6 @@ function updateMaps()
         end
     end
 end
+
+print(getMapMinPlayers("ttt_terrortownexamplemap"))
+print(getMapMaxPlayers("ttt_terrortownexamplemap"))
